f09bb69bd9ab73c242b8ddf832df627190a690c7
Mike Spreitzer <mspreitz@us.ibm.com>
2019-01-22 17:03:53 -0500

    Made controllers access API extension servers directly

--- staging/kos/cmd/controllers/ipam/main.go
+++ staging/kos/cmd/controllers/ipam/main.go
@@ -28,6 +28,7 @@ import (
 	"github.com/golang/glog"
 
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/util/workqueue"
 
 	kosclientset "k8s.io/examples/staging/kos/pkg/client/clientset/versioned"
@@ -64,6 +65,9 @@ func main() {
 	}
 	clientCfg.QPS = float32(clientQPS)
 	clientCfg.Burst = clientBurst
+	clientCfg.Host = "network-api:443"
+	// TODO: give our apiservers verifiable identities
+	clientCfg.TLSClientConfig = rest.TLSClientConfig{Insecure: true}
 
 	kcs, err := kosclientset.NewForConfig(clientCfg)
 	if err != nil {
